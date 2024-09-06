6e53501de76e78d8bb17e2922b43abc86a616468
Samu Ruuhonen <samu.ruuhonen@trimble.com>
2024-09-06 08:59:04 +0300

    Fixed nuget creation

--- NuGetter/BuildLocalNuget.bat
+++ NuGetter/BuildLocalNuget.bat
@@ -1,4 +1,4 @@
-
+REM header only library, no need to build anything
 REM MSBuild.exe .. /p:Configuration=140-DebugCRT /p:Platform=x64
 REM MSBuild.exe .. /p:Configuration=142-DebugCRT /p:Platform=x64
 REM MSBuild.exe .. /p:Configuration=140-Release /p:Platform=x64
@@ -9,4 +9,4 @@ set /p Build=<ver.txt
 set /a Build+=1
 echo %Build% >ver.txt
 
-c:\work\nuget.exe pack -Version 0.0.%Build%-test glm-trimble.nuspec
+c:\work\nuget.exe pack -Version 0.9.9.%Build% glm-trimble.nuspec
